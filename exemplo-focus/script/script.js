b2e5c6b8dad4e8729f675f071daba90bee6d8647
barbara-aguilar <bahlutz.design@gmail.com>
2018-09-18 16:46:18 -0300

    novos exercicios java-script

---
+++ exemplo-focus/script/script.js
@@ -0,0 +1,79 @@
+const inputNome = document.getElementById("cadastroInputNome");
+const inputEmail = document.getElementById("cadastroInputEmail");
+const inputEmailConfirm = document.getElementById("cadastroInputEmailConfirm");
+const inputPassword = document.getElementById("cadastroInputPassword");
+const inputPasswordConfirm = document.getElementById("cadastroInputPasswordConfirm");
+const inputPhone = document.getElementById("cadastroInputPhone");
+const inputArea = document.getElementById("cadastroInputArea");
+const inputLevel = document.getElementsByName("level")
+const inputNews = document.getElementById("cadastroInputNews")
+
+
+const button = document.querySelector(".cadastro__button");
+
+    button.addEventListener("click", function(evento){
+    evento.preventDefault();
+    
+
+    const optionSelect = inputArea.options
+    [inputArea.selectedIndex]
+    
+    if(optionSelect.value == "Front-End"){
+        document.querySelector("body").style.backgroundColor = "blue";
+    }else if(optionSelect.value == "Back-End"){
+        document.querySelector("body").style.backgroundColor = "green";
+    }else if(optionSelect.value == "UX Designer"){
+        document.querySelector("body").style.backgroundColor = "pink";
+    }else {
+        document.querySelector("body").style.backgroundColor = "orange";
+    }
+
+    
+    if(inputNome.value === undefined || inputNome.value === null ||
+        inputNome.value === "" || inputNome.value === " "){
+        inputNome.focus();
+        return false;
+    }else if( inputEmail.value === undefined || inputEmail.value === null ||
+        inputEmail.value === "" || inputEmail.value === " " ){
+        inputEmail.focus();
+        return false;
+    }else if(inputEmail.value !== inputEmailConfirm.value){
+        inputEmailConfirm.focus();
+        alert("Atenção os endereços de email não são compatíveis");
+    
+    }else if(inputPassword.value.length < 6){
+        inputPassword.focus();
+        alert("Atenção a senha deve conter no mínimo 7 caracteres");
+        
+    }else if(inputPassword.value !== inputPasswordConfirm.value){
+        alert("Atenção a senha e a confirmação não são compatíveis");
+    }else if(inputPhone.value === undefined || inputPhone.value === null ||
+        inputPhone.value === "" || inputPhone.value === " " ){
+        inputPhone.focus();
+        return false;
+    }
+
+    let radioItem;
+    for(let i = 0; i < inputLevel.length; i++){
+        if(inputLevel[i].checked){
+            console.log(inputLevel[i])
+            radioItem = inputLevel[i]
+        }
+    }
+
+    if(radioItem.value === "Junior"){
+        console.log("0 - 2 anos de experiência")
+    }else if(radioItem.value === "Pleno"){
+        console.log("2 - 5 anos de experiência")
+    }else{
+        console.log("5+ anos de experiência")
+    }
+
+
+    if(inputNews.checked === false){
+        alert("É uma pena que você não deseja receber nosso conteúdo exclusivo :c")
+    }
+    
+    
+
+    });
--- exercicio-contato/script/script.js
+++ exercicio-contato/script/script.js
@@ -1,12 +1,29 @@
 const inputNome = document.getElementById("contatoInputNome");
-const inputMail = document.getElementById("contatoInputEmail");
-const inputTel = document.getElementById("contatoInputPhone");
-const inputTexto = document.getElementById("contatoInputMessage");
+const inputEmail = document.getElementById("contatoInputEmail");
+const inputPhone = document.getElementById("contatoInputPhone");
+const inputMessage = document.getElementById("contatoInputMessage");
 const button = document.querySelector(".contato__button");
 
 
     button.addEventListener("click", function(evento){
         evento.preventDefault();
+
+        if(inputNome.value === undefined || inputNome.value === null ||
+            inputNome.value === "" || inputNome.value === " "){
+            inputNome.focus();
+            return false;
+        }else if( inputEmail.value === undefined || inputEmail.value === null ||
+            inputEmail.value === "" || inputEmail.value === " " ){
+            inputEmail.focus();
+            return false;    
+        }else if( inputPhone.value === undefined || inputPhone.value === null ||
+            inputPhone.value === "" || inputPhone.value === " " ){
+            inputPhone.focus();
+            return false;
+        }else {
+            inputMessage.focus();
+            return false;
+        }      
         
     });
 
